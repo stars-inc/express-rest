1c10d028d01172395f8e2b997943c0c5a5df8cdf
stars_inc <stars_inc@mail.ru>
2022-07-15 20:13:51 +0300

    feat: implement data recived to cart

--- models/user.js
+++ models/user.js
@@ -27,4 +27,24 @@ const userSchema = new Schema({
   }
 })
 
+userSchema.methods.addToCart = function(wish) {
+  const items = [...this.cart.items]
+  const index = items.findIndex(w => {
+    return w.wishId.toString() === wish._id.toString()
+  })
+
+  if (index >= 0) {
+    items[index].count += 1
+  } else {
+    items.push({
+      wishId: wish._id,
+      count: 1
+    })
+  }
+
+  this.cart = { items }
+
+  return this.save()
+}
+
 module.exports = model('User', userSchema)
